43548ba4389a178c03e2ac42b71183026e04dd67
Malte <wombatconrad@gmail.com>
2023-10-09 15:23:06 +0200

    Update Rabbit.cs

--- WonderfulRabbitsApi/Entities/Rabbit.cs
+++ WonderfulRabbitsApi/Entities/Rabbit.cs
@@ -5,6 +5,7 @@ namespace WonderfulRabbitsApi.Entities
     public class Rabbit
     {
         public int Id { get; set; }
+        [StringLength(16, ErrorMessage = "The {0} value cannot exceed {1} characters. ")]
         public string Name { get; set; }
         public DateTime? Birthdate { get; set; }
         public virtual User User { get; set; }
