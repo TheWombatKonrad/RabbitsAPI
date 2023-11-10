ed0163b2c248235d18d326e612153319d22e22bc
TheWombatKonrad <wombatconrad@gmail.com>
2023-11-10 14:45:25 +0100

    changed name & added property

--- WonderfulRabbitsApi/Models/Images/UploadImageModel.cs
+++ WonderfulRabbitsApi/Models/Images/UploadImageModel.cs
@@ -3,12 +3,11 @@ using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 
 namespace WonderfulRabbitsApi.Models.Images;
 
-public class RegisterImageModel
+public class UploadImageModel
 {
     public int RabbitId { get; set; }
     public string Title { get; set; }
     public string Base64ImageData { get; set; }
-    public string FileName { get; set; }
     public string FileExtension { get; set; }
 
 }
